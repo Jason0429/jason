997f0690aa59dde9d407554ad3ff8c7664e5dd52
Jason Cheung <109165559+jason-palapa@users.noreply.github.com>
2023-07-01 13:19:02 -0400

    added suidPlugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
+import suidPlugin from '@suid/vite-plugin';
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+
 // import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
@@ -8,7 +10,8 @@ export default defineConfig({
     Uncomment the following line to enable solid-devtools.
     For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
     */
-    // devtools(),
+    // devtools(),]
+    suidPlugin(),
     solidPlugin(),
   ],
   server: {
