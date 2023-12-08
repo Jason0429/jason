749c701cf4dceb34a9eab29365fe9cef4f786950
Jason0429 <jasoncheung0429@gmail.com>
2023-12-08 02:28:17 -0500

    added vercel adapter

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,12 @@ import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 import tsconfigPaths from "vite-tsconfig-paths";
 import suidPlugin from "@suid/vite-plugin";
+import vercel from "solid-start-vercel";
 
 export default defineConfig({
   plugins: [tsconfigPaths(), solid({
-    ssr: false
+    ssr: false,
+    adapter: vercel()
   }), suidPlugin(), solidPlugin()],
   build: {
     target: "esnext"
